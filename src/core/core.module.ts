5a155399f5a5220005ee3074ec32b23564af0566
sokolw <sokolw21@gmail.com>
2023-02-05 13:56:46 +0300

    feat: provide repos

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ArtistRepositoryService } from './repository/services/artist-repository.service';
+import { TrackRepositoryService } from './repository/services/track-repository.service';
 import { UserRepositoryService } from './repository/services/user-repository.service';
+import { AlbumRepositoryService } from './repository/services/album-repository.service';
+import { FavsRepositoryService } from './repository/services/favs-repository.service';
 
 @Module({
-  providers: [UserRepositoryService],
-  exports: [UserRepositoryService],
+  providers: [
+    UserRepositoryService,
+    TrackRepositoryService,
+    ArtistRepositoryService,
+    AlbumRepositoryService,
+    FavsRepositoryService,
+  ],
+  exports: [
+    UserRepositoryService,
+    TrackRepositoryService,
+    ArtistRepositoryService,
+    AlbumRepositoryService,
+    FavsRepositoryService,
+  ],
 })
 export class CoreModule {}
