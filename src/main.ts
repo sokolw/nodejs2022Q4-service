710705888172ef4131d6d66a45b57df6b809b4dc
sokolw <sokolw21@gmail.com>
2023-02-06 13:28:48 +0300

    fix: route name

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('doc', app, document);
 
   await app.listen(+process.env.PORT || 4000);
 }
