81d5d94a85e1c9102312e33a34df1df2f57c852d
sokolw <sokolw21@gmail.com>
2023-02-16 11:40:51 +0300

    feat: rework now use db

--- src/favs/favs.module.ts
+++ src/favs/favs.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { albumProviders } from 'src/album/album.providers';
+import { artistProviders } from 'src/artist/artist.providers';
 import { CoreModule } from 'src/core/core.module';
+import { DatabaseModule } from 'src/database/database.module';
+import { trackProviders } from 'src/track/track.providers';
 import { FavsController } from './favs.controller';
 import { FavsService } from './favs.service';
 
 @Module({
-  imports: [CoreModule],
+  imports: [CoreModule, DatabaseModule],
   controllers: [FavsController],
-  providers: [FavsService],
+  providers: [
+    ...trackProviders,
+    ...albumProviders,
+    ...artistProviders,
+    FavsService,
+  ],
 })
 export class FavsModule {}
--- src/favs/favs.service.ts
+++ src/favs/favs.service.ts
@@ -1,5 +1,4 @@
-import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
-import { FavsRepositoryService } from './../core/repository/services/favs-repository.service';
+import { Injectable, HttpException, HttpStatus, Inject } from '@nestjs/common';
 import { validate } from 'uuid';
 import {
   ALBUM_NOT_EXIST,
@@ -7,116 +6,160 @@ import {
   INVALID_ID,
   TRACK_NOT_EXIST,
 } from 'src/core/constants';
-import { TrackRepositoryService } from 'src/core/repository/services/track-repository.service';
-import { ArtistRepositoryService } from 'src/core/repository/services/artist-repository.service';
-import { AlbumRepositoryService } from 'src/core/repository/services/album-repository.service';
+import { Repository } from 'typeorm';
+import { Track } from 'src/track/track.entity';
+import { Artist } from 'src/artist/artist.entity';
+import { Album } from 'src/album/album.entity';
+import { FavoritesResponse } from './classes/favorites-response';
+import { TrackService } from 'src/track/track.service';
+import { ArtistService } from 'src/artist/artist.service';
+import { AlbumService } from 'src/album/album.service';
 
 @Injectable()
 export class FavsService {
   constructor(
-    private favsRepositoryService: FavsRepositoryService,
-    private trackRepositoryService: TrackRepositoryService,
-    private artistRepositoryService: ArtistRepositoryService,
-    private albumRepositoryService: AlbumRepositoryService,
+    @Inject('TRACK_REPOSITORY')
+    private trackRepository: Repository<Track>,
+    @Inject('ARTIST_REPOSITORY')
+    private artistRepository: Repository<Artist>,
+    @Inject('ALBUM_REPOSITORY')
+    private albumRepository: Repository<Album>,
   ) {}
 
-  async getAllFavs() {
-    return this.favsRepositoryService.getAll();
+  async getAllFavs(): Promise<FavoritesResponse> {
+    const favsTracks = await this.trackRepository.find({
+      where: { isFavorite: true },
+      relations: { album: true, artist: true },
+    });
+    const favsArtists = await this.artistRepository.find({
+      where: { isFavorite: true },
+    });
+    const favsAlbums = await this.albumRepository.find({
+      where: { isFavorite: true },
+      relations: { artist: true },
+    });
+    return this.buildFavsResponse(favsTracks, favsArtists, favsAlbums);
   }
 
-  async addTrack(id: string) {
+  async addTrack(id: string): Promise<void> {
     if (!validate(id)) {
       throw new HttpException({ message: INVALID_ID }, HttpStatus.BAD_REQUEST);
     }
 
-    const existTrack = this.trackRepositoryService.getById(id);
-    if (!existTrack) {
+    const track = await this.trackRepository.findOneBy({ id });
+    if (!track) {
       throw new HttpException(
         { message: TRACK_NOT_EXIST },
         HttpStatus.UNPROCESSABLE_ENTITY,
       );
     }
 
-    this.favsRepositoryService.addTrackId(id);
+    await this.trackRepository.save(Object.assign(track, { isFavorite: true }));
   }
 
-  async delTrack(id: string) {
+  async delTrack(id: string): Promise<void> {
     if (!validate(id)) {
       throw new HttpException({ message: INVALID_ID }, HttpStatus.BAD_REQUEST);
     }
 
-    const existTrack = this.trackRepositoryService.getById(id);
-    if (!existTrack) {
+    const track = await this.trackRepository.findOneBy({ id });
+    if (!track) {
       throw new HttpException(
         { message: TRACK_NOT_EXIST },
         HttpStatus.NOT_FOUND,
       );
     }
 
-    this.favsRepositoryService.delTrackId(id);
+    await this.trackRepository.save(
+      Object.assign(track, { isFavorite: false }),
+    );
   }
 
-  async addAlbum(id: string) {
+  async addAlbum(id: string): Promise<void> {
     if (!validate(id)) {
       throw new HttpException({ message: INVALID_ID }, HttpStatus.BAD_REQUEST);
     }
 
-    const existAlbum = this.albumRepositoryService.getById(id);
-    if (!existAlbum) {
+    const album = await this.albumRepository.findOneBy({ id });
+    if (!album) {
       throw new HttpException(
         { message: ALBUM_NOT_EXIST },
         HttpStatus.UNPROCESSABLE_ENTITY,
       );
     }
 
-    this.favsRepositoryService.addAlbumId(id);
+    await this.albumRepository.save(Object.assign(album, { isFavorite: true }));
   }
 
-  async delAlbum(id: string) {
+  async delAlbum(id: string): Promise<void> {
     if (!validate(id)) {
       throw new HttpException({ message: INVALID_ID }, HttpStatus.BAD_REQUEST);
     }
 
-    const existAlbum = this.albumRepositoryService.getById(id);
-    if (!existAlbum) {
+    const album = await this.albumRepository.findOneBy({ id });
+    if (!album) {
       throw new HttpException(
         { message: ALBUM_NOT_EXIST },
         HttpStatus.NOT_FOUND,
       );
     }
 
-    this.favsRepositoryService.delAlbumId(id);
+    await this.albumRepository.save(
+      Object.assign(album, { isFavorite: false }),
+    );
   }
 
-  async addArtist(id: string) {
+  async addArtist(id: string): Promise<void> {
     if (!validate(id)) {
       throw new HttpException({ message: INVALID_ID }, HttpStatus.BAD_REQUEST);
     }
 
-    const existArtist = this.artistRepositoryService.getById(id);
-    if (!existArtist) {
+    const artist = await this.artistRepository.findOneBy({ id });
+    if (!artist) {
       throw new HttpException(
         { message: ARTIST_NOT_EXIST },
         HttpStatus.UNPROCESSABLE_ENTITY,
       );
     }
 
-    this.favsRepositoryService.addArtistId(id);
+    await this.artistRepository.save(
+      Object.assign(artist, { isFavorite: true }),
+    );
   }
 
-  async delArtist(id: string) {
+  async delArtist(id: string): Promise<void> {
     if (!validate(id)) {
       throw new HttpException({ message: INVALID_ID }, HttpStatus.BAD_REQUEST);
     }
 
-    const existArtist = this.artistRepositoryService.getById(id);
-    if (!existArtist) {
+    const artist = await this.artistRepository.findOneBy({ id });
+    if (!artist) {
       throw new HttpException(
         { message: ARTIST_NOT_EXIST },
         HttpStatus.NOT_FOUND,
       );
     }
 
-    this.favsRepositoryService.delArtistId(id);
+    await this.artistRepository.save(
+      Object.assign(artist, { isFavorite: false }),
+    );
+  }
+
+  private buildFavsResponse(
+    favsTracks: Track[],
+    favsArtists: Artist[],
+    favsAlbums: Album[],
+  ): FavoritesResponse {
+    return {
+      artists: favsArtists.map((artist) =>
+        ArtistService.transformArtistEntity(artist),
+      ),
+      albums: favsAlbums.map((album) =>
+        AlbumService.transformAlbumEntity(album),
+      ),
+      tracks: favsTracks.map((track) =>
+        TrackService.transformTrackEntity(track),
+      ),
+    };
   }
 }
