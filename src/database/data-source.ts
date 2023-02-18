c39c92985877c33d1cecad1f0220e4db134ec33c
sokolw <sokolw21@gmail.com>
2023-02-18 10:53:58 +0300

    feat: add generation migrations

---
+++ src/database/data-source.ts
@@ -0,0 +1,32 @@
+import { Album } from 'src/album/album.entity';
+import { Artist } from 'src/artist/artist.entity';
+import { Track } from 'src/track/track.entity';
+import { User } from 'src/user/user.entity';
+import { DataSource } from 'typeorm';
+import 'dotenv/config';
+
+const isNestApp =
+  process.env.npm_lifecycle_script.includes('nest') ||
+  process.env.npm_lifecycle_script.includes('node dist/main');
+
+const migrationsImporterWrapper = async () => {
+  const migrations = await (await import('./migrations-importer')).default;
+  return migrations.map((item) => Object.values(item)[0]);
+};
+
+const dataSource = (async () =>
+  new DataSource({
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: +process.env.POSTGRES_PORT,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    entities: [User, Artist, Album, Track],
+    synchronize: false,
+    migrationsRun: false,
+    migrations: !isNestApp ? await migrationsImporterWrapper() : [],
+    migrationsTableName: 'migration-home-library',
+  }))();
+
+export default dataSource;
--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,25 +1,10 @@
-import { Album } from 'src/album/album.entity';
-import { Artist } from 'src/artist/artist.entity';
-import { Track } from 'src/track/track.entity';
-import { User } from 'src/user/user.entity';
-import { DataSource } from 'typeorm';
+import dataSource from './data-source';
 
 export const databaseProviders = [
   {
     provide: 'DATA_SOURCE',
     useFactory: async () => {
-      const dataSource = new DataSource({
-        type: 'postgres',
-        host: process.env.POSTGRES_HOST,
-        port: +process.env.POSTGRES_PORT,
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
-        entities: [User, Artist, Album, Track],
-        synchronize: true,
-      });
-
-      return dataSource.initialize();
+      return (await dataSource).initialize();
     },
   },
 ];
