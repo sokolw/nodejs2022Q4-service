e31be4d7b952109d2531a16453f683a27f907c4a
sokolw <sokolw21@gmail.com>
2023-02-27 16:33:45 +0300

    feat: implement HttpExceptionFilter

---
+++ src/core/filters/http-exception.filter.ts
@@ -0,0 +1,51 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { HttpAdapterHost } from '@nestjs/core';
+import { CustomLoggerService } from '../services/custom-logger.service';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  constructor(
+    private readonly httpAdapterHost: HttpAdapterHost,
+    private customLoggerService: CustomLoggerService,
+  ) {}
+
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    const { httpAdapter } = this.httpAdapterHost;
+
+    const httpStatus =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const responseBody = {
+      statusCode: httpStatus,
+      timestamp: new Date().toISOString(),
+      path: httpAdapter.getRequestUrl(request),
+    };
+
+    if (httpStatus >= 500) {
+      this.customLoggerService.error(
+        (exception as Error).message,
+        (exception as Error).stack,
+      );
+    } else if (httpStatus >= 400) {
+      this.customLoggerService.warn(
+        (exception as Error).message,
+        (exception as Error).stack,
+      );
+    }
+
+    httpAdapter.reply(response, responseBody, httpStatus);
+  }
+}
