e69b3ea79db092b3ec8f1dbdf1f5c9b0a3305101
sokolw <sokolw21@gmail.com>
2023-02-27 16:36:26 +0300

    feat: move user repos connection

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/database/database.module';
+import { userProviders } from 'src/user/user.providers';
 import { CustomLoggerService } from './services/custom-logger.service';
 import { LogsToFileService } from './services/logs-to-file.service';
 
 @Module({
-  providers: [CustomLoggerService, LogsToFileService],
-  exports: [CustomLoggerService],
+  imports: [DatabaseModule],
+  providers: [...userProviders, CustomLoggerService, LogsToFileService],
+  exports: [CustomLoggerService, DatabaseModule, ...userProviders],
 })
 export class CoreModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CoreModule } from 'src/core/core.module';
-import { DatabaseModule } from 'src/database/database.module';
 import { UserController } from './user.controller';
-import { userProviders } from './user.providers';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [CoreModule, DatabaseModule],
+  imports: [CoreModule],
   controllers: [UserController],
-  providers: [...userProviders, UserService],
+  providers: [UserService],
   exports: [UserService],
 })
 export class UserModule {}
